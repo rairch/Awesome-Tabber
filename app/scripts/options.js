b493b861ee4db62338fbd07f62f757fd0e884816
Patrick Clancey <patrick.clancey@cognifide.com>
2014-11-08 15:52:18 +0000

    Add basic options

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,52 @@
-'use strict';
+(function($, document, window, undefined){
+    "use strict";
 
-console.log('\'Allo \'Allo! Option');
+
+    var options = {backgroundPage : null, active : false},
+        manifest = chrome.runtime.getManifest();
+
+
+    options.init = function(){
+    	$("h1").text( manifest.name + " Settings");
+        $(".form__lable--title").text( manifest.name )
+
+        $("input[name=active]").on("click",function(e){
+            var $el = $(e.target);
+            // TODO; options.backgoundPage.at.setSetting({  the active setting  })
+            if ($el.prop("checked")){
+            	options.backgroundPage.at.start();
+            } else {
+            	options.backgroundPage.at.stop();
+            }
+        });
+        $("input[name=active]").prop("checked", options.active);
+        $("input[type=range]").on('input change',function(e){
+            var $el = $(e.target),
+                val = $el.val(),
+                $parent = $el.closest("div"),
+                $display = $parent.find(".form__show-value");
+
+            $display.text( val );
+            // TODO; options.backgoundPage.at.setSetting({  the interval setting  })
+        });
+        $("input[name=interval]").val( options.interval ).trigger('change');
+        
+        setTimeout(function(){
+            $("body").addClass( "ready" );
+        },100);
+    }
+    $(document).ready(function(){
+        // IF USING EVENT PAGE
+        // chrome.runtime.getBackgroundPage(function(bg){
+        //     options.backgroundPage = bg;
+        //     options.active = options.backgroundPage.at.getSetting({"key":"active","type":"boolean"});
+        //     options.init();
+        // })
+        options.backgroundPage = chrome.extension.getBackgroundPage();
+        options.active = true ;// options.backgroundPage.at.getSetting({"key":"active","type":"boolean"});
+        options.interval = 50 ; // TODO GET FROM BACKGROUND
+        options.init();
+    });
+    
+
+})(window.$, document, window);
--- app/scripts/popup.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-console.log('Awesome Tabber Popup');
-
-var background = chrome.extension.getBackgroundPage();
-background.at.moveNext();
